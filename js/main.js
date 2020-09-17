3d5b92bf2034f2825db1ab05a98e6baee36c011e
valerio <valerio.deprosperi@gmail.com>
2020-09-17 18:18:03 +0200

    problemi chiamata ajax

--- js/main.js
+++ js/main.js
@@ -3,4 +3,10 @@ $(document).ready(function() {
     'url': 'https://flynn.boolean.careers/exercises/api/array/music',
     'method': 'GET',
     'success': function(data)
+    printAlbum(data.response);
+  },
+  'error': function(request, state, errors) {
+   alert('Errore ' + errors);
+        }
+      });
 });
